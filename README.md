2260423dcbbd3a6f297895c31c4dabf2a27ab784
Karina Nagieva <nagieva1995@list.ru>
2022-09-04 22:42:17 +0500

    added tests

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/xk17c603gtgh2f09?svg=true)](https://ci.appveyor.com/project/KarinaNM27/orderofcart)
--- src/test/java/OrderOfCartTest.java
+++ src/test/java/OrderOfCartTest.java
@@ -2,18 +2,35 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class OrderOfCartTest {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     @BeforeAll
     static void setupAll() {
         System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
+
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
 
     }
 
@@ -25,11 +42,19 @@ class OrderOfCartTest {
     @AfterEach
     void teardown() {
         driver.quit();
+        driver = null;
     }
 
     @Test
     void test() {
-        // Your test logic here
+        driver.get("http://localhost:7777");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иванова Мария");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79123456789");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
+
 
 }
