bc875edcdb55e549fdb7241aa7defab194257161
Karina Nagieva <nagieva1995@list.ru>
2022-09-04 13:53:07 +0500

    first setup

---
+++ src/test/java/OrderOfCartTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class OrderOfCartTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Your test logic here
+    }
+
+}
