ae2693b1805627ec0c66fa69a246adf71e0bdaf8
Karina Nagieva <nagieva1995@list.ru>
2022-09-06 20:37:32 +0500

    added test9

--- src/test/java/OrderOfCartTest.java
+++ src/test/java/OrderOfCartTest.java
@@ -21,13 +21,6 @@ class OrderOfCartTest {
     static void setupAll() {
         WebDriverManager.chromedriver().setup();
 
-
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
-//        driver = new ChromeDriver(options);
-
     }
 
 
