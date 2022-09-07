45870051512c0b1f59ca525248838115ec26beb7
Karina Nagieva <nagieva1995@list.ru>
2022-09-07 20:09:02 +0500

    added test11

--- src/test/java/OrderOfCartTest.java
+++ src/test/java/OrderOfCartTest.java
@@ -36,17 +36,7 @@ class OrderOfCartTest {
     }
 
 
-    @Test
-    void test() {
 
-        driver.get("http://localhost:7777");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иванова Мария");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79123456789");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.className("button_view_extra")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
-        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
-    }
 
 
     @AfterEach
@@ -55,5 +45,17 @@ class OrderOfCartTest {
         driver = null;
     }
 
+
+    @Test
+    void test() {
+
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иванова Мария");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79123456789");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
 
 }
